cc342b27a0af0a19241c2bc119ad3713e538eae9
Kazik Pogoda <morisil@xemantic.com>
2021-01-23 02:44:25 +0100

    build: compiles almost almost_pseudo_random.c

--- build-c.sh
+++ build-c.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env sh
 
 mkdir -p build/c
-gcc -O3 -o build/c/java_2_times_faster_than_c src/main/c/java_2_times_faster_than_c.c
+gcc -O3 -o build/c/almost_pseudo_random.c src/main/c/almost_pseudo_random.c -lm
+gcc -O3 -o build/c/java_2_times_faster_than_c src/main/c/java_2_times_faster_than_c.c -lm
